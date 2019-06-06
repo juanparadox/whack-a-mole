e0c8a6b781a0b62755b0e11718f11faf1d84a7ee
Juan Bernal <juanparadox@juans-mbp.lan>
2019-06-05 23:23:44 -0400

    What a whacky game...

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # whack-a-mole
 Vanilla JS whack-a-mole game! Dead simple.
+
+Start the game by clicking on 'start' from the welcome screen.
+
+To stop the game (not pause), click the 'stop' button at the bottom of the screen.
+
+To reset the game, click the 'reset' button at the bottom of the screen.
+
+To start the game after it has been reset, 'click the 'start' button at the bottom of the screen.
+
+
+Have fun and whack-a-mole!
---
+++ scripts.js
@@ -0,0 +1,111 @@
+let moleTimeouts = [];
+let score = 0;
+let highScore = 0;
+let gameTime = 30;
+let lengthInterval;
+
+/** Gets milliseconds based on a ceiling */
+function getMilliseconds(ceiling) {
+    return Math.random() * ceiling * 1000;
+}
+
+/** Hides a mole */
+function hideMole(mole) {
+    const timeout = getMilliseconds(3) + 1000;
+    moleTimeouts.push(setTimeout(() => {
+        // Remove class to hide the mole
+        mole.classList.remove('--up');
+        showMole(mole);
+    }, timeout));
+}
+
+/** Shows a mole */
+function showMole(mole) {
+    const timeInterval = getMilliseconds(10);
+    moleTimeouts.push(setTimeout(() => {
+        // Add class to show the mole
+        mole.classList.add('--up');
+        hideMole(mole);
+    }, timeInterval));
+}
+
+/** Updates the current score in the DOM */
+function updateHighScore() {
+    if (score > highScore) {
+        highScore = score;
+        document.getElementById('high-score').innerHTML = highScore;
+    }
+}
+
+/** Sets the current score */
+function setCurrentScore(currentScore) {
+    document.getElementById('current-score').innerHTML = currentScore;
+}
+
+/** Updates the current score in the DOM */
+function updateCurrentScore() {
+    score += 100;
+    updateHighScore();
+    setCurrentScore(score);
+}
+
+/** Handles a mole click by hiding the mole and updating the score */
+function handleMoleClick({ target }) {
+    target.classList.remove('--up');
+    updateCurrentScore();
+}
+
+/** Set count down time */
+function setCountdown(countdown) {
+    document.getElementById('time').innerHTML = countdown;
+}
+
+/** Updates the timer count down */
+function updateCountdown() {
+    gameTime--;
+    setCountdown(gameTime);
+    // If time is up, at 0
+    if (!gameTime) {
+        setCountdown(`Time's up!`);
+        clearInterval(lengthInterval);
+        handleStopGame();
+    }
+}
+
+/** Handles the click of the start button and begins the game */
+function handleStartGame() {
+    // Hide welcome screen
+    document.getElementById('welcome-screen').classList.add('--hidden');
+    // Show game
+    document.getElementById('game').classList.remove('--hidden');
+    const moles = document.querySelectorAll('.mole');
+    // Game length timer
+    lengthInterval = setInterval(updateCountdown, 1000);
+    // Add intervals and click events for each mole
+    moles.forEach(mole => {
+        mole.addEventListener('click', handleMoleClick);
+        showMole(mole);
+    });
+    // Disabled start button
+    document.getElementById('start-button').disabled = 'true';
+}
+
+/** Stops the game in motion */
+function handleStopGame() {
+    // Clear all timeouts
+    moleTimeouts.forEach(timeout => clearTimeout(timeout));
+    clearInterval(lengthInterval);
+}
+
+/** Resets the game to starting point */
+function handleResetGame() {
+    const moles = document.querySelectorAll('.mole');
+    moles.forEach(mole => mole.classList.remove('--up'));
+    clearInterval(lengthInterval);
+    handleStopGame();
+    gameTime = 30;
+    score = 0;
+    setCurrentScore(score);
+    setCountdown(gameTime);
+    document.getElementById('start-button').removeAttribute('disabled');
+}
